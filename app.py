eeb4e334bf32d1c735673770ebf1dbde6c78a901
Fazl Rahman Sabawoon <sabawoonrahman942@gmail.com>
2026-08-01 22:22:05 +0000

    Initial DevOps project

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import psycopg2
+
+DB_HOST = os.getenv("DB_HOST", "my-service-db")
+DB_NAME = os.getenv("POSTGRES_DB", "myservice")
+DB_USER = os.getenv("POSTGRES_USER", "myuser")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "mypassword")
+
+
+def check_database():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT version();")
+        version = cursor.fetchone()[0]
+
+        print("PostgreSQL bağlantısı başarılı.")
+        print(f"Database: {DB_NAME}")
+        print(f"PostgreSQL: {version}")
+
+        cursor.close()
+        conn.close()
+
+    except Exception as e:
+        print(f"PostgreSQL bağlantı hatası: {e}")
+
+
+check_database()
+
+server = HTTPServer(("0.0.0.0", 9090), SimpleHTTPRequestHandler)
+
+print("Server started on port 9090")
+
+server.serve_forever()
